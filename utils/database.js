9ec9e3c5044636d2e99b1c86f85f61941437e62c
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-02-07 23:21:57 +0500

    Remove unused options from database connection

--- utils/database.js
+++ utils/database.js
@@ -11,8 +11,6 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: 'promptopia',
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         });
         isConnected = true;
     }
