1220cc9c295e981f10e45ee29b94c83c7bfc5bf5
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-02-07 19:35:15 +0500

    Changed Homapage Layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,28 @@
 import "@styles/globals.css";
+
 import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
-  description: "Descover and Share AI prompts",
+  description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang="en">
-      <body>
-        <div className="main">
-          <div className="gradient" />
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
         </div>
-        <main className="app">
+
+        <main className='app'>
           <Nav />
           {children}
-          </main>
-      </body>
-    </html>
-  );
-};
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
