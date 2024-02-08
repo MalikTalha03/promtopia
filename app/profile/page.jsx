f1b1bb1b389eb8da2fdd17bb09cf20871b1c8d3b
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-02-08 09:57:06 +0500

    Refactor profile page component

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,33 +1,34 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Profile from "@components/Profile";
 
 const page = () => {
-    const { data: session } = useSession();
-    const [posts, setPosts] = useState([]);
-    const handleEdit = (id) => {
-    }
-    const handleDelete = (id) => {
-    }
-    useEffect(() => {
-        const fetchData = async () => {
-          const response = await fetch(`/api/users/${session?.user.id}/posts`);
-          const data = await response.json();
-          setPosts(data);
-        };
-        if(session?.user.id) fetchData();
-      }, [session?.user.id]);
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = (post) => {};
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchData();
+  }, [session?.user.id]);
   return (
-    <Profile 
-    name ="My Profile"
-    desc = "This is your profile"
-    data ={posts}
-    handleDelete = {handleDelete}
-    handleEdit = {handleEdit}
+    <Profile
+      name="My Profile"
+      desc="This is your profile"
+      data={posts}
+      handleDelete={handleDelete}
+      handleEdit={handleEdit}
     />
-  )
-}
+  );
+};
 
-export default page
+export default page;
