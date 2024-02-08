1194314164ab11381177c2c3ef5a1fe2b21116a2
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-02-08 11:16:34 +0500

    Refactor UpdatePrompt component and fix variable naming

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -5,29 +5,35 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
-const EditPrompt = () => {
+const UpdatePrompt = () => {
   const router = useRouter();
-    const searchParams = useSearchParams();
-    const promptID = searchParams.get("id");
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+
+  const [post, setPost] = useState({ prompt: "", tag: "", });
   const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
 
   useEffect(() => {
-    const getDetails = async () => {
-        const response = await fetch(`/api/prompt/${promptID}`);
-        const data = await response.json();
-        setPost({ prompt: data.prompt, tag: data.tag });
-    }
-    if (promptID) getDetails();
-    }, [promptID]);
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
 
-  const editPrompt = async (e) => {
+    if (promptId) getPromptDetails();
+  }, [promptId]);
+
+  const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
-    if (!promptID) alert("Prompt not found");
+
+    if (!promptId) return alert("Missing PromptId!");
 
     try {
-      const response = await fetch(`/api/prompt/${promptID}`, {
+      const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
         body: JSON.stringify({
           prompt: post.prompt,
@@ -51,9 +57,9 @@ const EditPrompt = () => {
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={editPrompt}
+      handleSubmit={updatePrompt}
     />
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
